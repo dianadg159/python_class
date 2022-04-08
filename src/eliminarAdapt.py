1a671e451d17ff4e643ed1fb7aea06b165b9bc1a
Diana <diana.delgado.gtz@gmail.com>
2022-04-07 23:58:12 -0500

    Programa eliminarAdapt.py

--- src/eliminarAdapt.py
+++ src/eliminarAdapt.py
@@ -1,13 +1,13 @@
 '''
 NAME
     Eliminar adaptador de secuencias
-    
+
 VERSION
     1.0
-    
+
 AUTOR
     Diana Delgado Gutierrez
-    
+
 DESCRIPTION
     A partir de un archivo .txt crear otro archivo con la misma secuencia
     sin unas regiones de la secuencia (adaptadores).
@@ -26,14 +26,31 @@ INPUT
 
 OUTPUT
     4_output_noAdapters.txt
-    
-'''
-# Crear el archivo de la secuencia con los adaptadores.
 
-# Leer el archivo línea por linea para poder recorrer
-#   el contenido con un ciclo
+'''
+# Crear el archivo de la secuencia.
+archivo = open("data/4_input_adapters.txt", "w")
+linea1 = "ATTCGATTATAAGCTCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATC\n"
+linea2 = "ATTCGATTATAAGCACTGATCGATCGATCGATCGATCGATGCTATCGTCGT\n"
+linea3 = "ATTCGATTATAAGCATCGATCACGATCTATCGTACGTATGCATATCGATATCGATCGTAGTC \n"
+linea4 = "ATTCGATTATAAGCACTATCGATGATCTAGCTACGATCGTAGCTGTA\n"
+linea5 = "ATTCGATTATAAGCACTAGCTAGTCTCGATGCATGATCAGCTTAGCTGATGATGCTATGCA\n"
+archivo.write(linea1)
+archivo.write(linea2)
+archivo.write(linea3)
+archivo.write(linea4)
+archivo.write(linea5)
+archivo.close()
+
+# Leer el archivo línea por línea.
+contenido = open("data/4_input_adapters.txt", "r")
+secuencias = contenido.readlines()
+contenido.close()
 
 # Crear un archivo output.
+secNoAdapt = open("data/4_output_adapters.txt", "w")
 
-# Recorrer la lista para seleccionar un rango de la secuencia
-#    a partir del elemento 14 y escribirla en archivo output.
+# Recorrer la lista y eliminar adaptador.
+for sec in secuencias:
+    secNoAdapt.write(str(sec[14:-1]))
+    secNoAdapt.write("\n")
