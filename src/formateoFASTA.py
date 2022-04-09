dc14550a69b8925bc2deaeb1961b4135ede6efd1
Diana <diana.delgado.gtz@gmail.com>
2022-04-08 22:03:34 -0500

    Se agrega pseudocódigo y encabezado de formateoFASTA.py

---
+++ src/formateoFASTA.py
@@ -0,0 +1,46 @@
+'''
+NAME
+    Formatear secuencia a Fasta
+    
+VERSION
+    1.0
+
+AUTOR
+    Diana Delgado Gutiérrez 
+
+DESCRIPTION
+    Dado un archivo dna_sequences.txt de secuencias
+    convertirlas a formato FastA en otro archivo output
+    dna_sequences.FASTA
+
+CATEGORY
+    FASTA format
+
+USAGE
+    py src/formatoFASTA.py
+
+ARGUMENTS
+    None
+
+SOFTWARE REQUIREMENTS
+    Python 3.10
+
+INPUT
+    data/dna_sequences.txt
+
+OUTPUT
+    results/dna_sequences.FASTA
+'''
+# Crear el archivo .txt con las secuencias sin formato.
+
+# Abrir el archivo para leer línea por línea y guardar el contenido
+# en una lista.
+
+# Cerrar el archivo.
+
+# Crear un archivo output .FASTA
+
+# Cortar la primera parte de la secuencia para usarla de encabezado.
+
+# Editar cada una de las secuencias a un formato FASTA y escribirlas
+# en el archivo .FASTA
