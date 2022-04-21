9353839c2cd4f142282a307508576fffbea001cc
Diana <diana.delgado.gtz@gmail.com>
2022-04-21 13:13:25 -0500

    Se agrega un while a src/piedraPapelTijera.py

--- src/piedraPapelTijera.py
+++ src/piedraPapelTijera.py
@@ -3,7 +3,7 @@ NAME
     Juego piedra, papel o tijera
 
 VERSION
-    1.1
+    1.2
 
 AUTOR
     Diana Delgado Gutierrez
@@ -33,45 +33,50 @@ OUTPUT
 # Definir las opciones validas a elegir.
 import random
 accionesPosibles = ["piedra", "papel", "tijera"]
+playAgain = "si"
 
-# Pedir al usuario para su elección.
-print("¡Bienvenidx al juego de piedra, papel o tijera!\n")
-print("Teclea tu nombre: ")
-nombre = input()
-print(f"{nombre}, a la de 3, elige una opción (piedra, papel, tijera): ")
-eleccionUsuario = input().lower()
+# Hacer un ciclo para volver a jugar
+while playAgain.lower() == "si":
+    # Pedir al usuario para su elección.
+    print("¡Bienvenidx al juego de piedra, papel o tijera!\n")
+    print("Teclea tu nombre: ")
+    nombre = input()
+    print(f"{nombre}, a la de 3, elige una opción (piedra, papel, tijera): ")
+    eleccionUsuario = input().lower()
 
-# Elección aleatoria de la computadora.
-eleccionCompu = random.choice(accionesPosibles)
-print(f"\nTu elección fue: {eleccionUsuario}")
-print(f"La computadora elige: {eleccionCompu}\n")
+    # Elección aleatoria de la computadora.
+    eleccionCompu = random.choice(accionesPosibles)
+    print(f"\nTu elección fue: {eleccionUsuario}")
+    print(f"La computadora elige: {eleccionCompu}\n")
 
-# Comparar los resultados.
-# Los dos elegieron lo mismo:
-if (eleccionCompu == eleccionUsuario):
-    print("¡Empate!")
+    # Comparar los resultados.
+    # Los dos elegieron lo mismo:
+    if (eleccionCompu == eleccionUsuario):
+        print("¡Empate!")
 
-# Papel pierde ante tijera:
-elif ((eleccionCompu == "tijera") & (eleccionUsuario == "papel")):
-    print(f"Perdiste {nombre} :( ¡Inténtalo de nuevo!")
-# papel gana ante piedra:
-elif ((eleccionCompu == "piedra") & (eleccionUsuario == "papel")):
-    print(f"¡Ganaste {nombre}!")
+    # Papel pierde ante tijera:
+    elif ((eleccionCompu == "tijera") & (eleccionUsuario == "papel")):
+        print(f"Perdiste {nombre} :( ¡Inténtalo de nuevo!")
+    # papel gana ante piedra:
+    elif ((eleccionCompu == "piedra") & (eleccionUsuario == "papel")):
+        print(f"¡Ganaste {nombre}!")
 
-# tijera pierde ante piedra:
-elif ((eleccionCompu == "piedra") & (eleccionUsuario == "tijera")):
-    print(f"Perdiste {nombre} :( ¡Inténtalo de nuevo!")
-# tijera gana ante papel:
-elif ((eleccionCompu == "papel") & (eleccionUsuario == "tijera")):
-    print(f"¡Ganaste {nombre}!")
+    # tijera pierde ante piedra:
+    elif ((eleccionCompu == "piedra") & (eleccionUsuario == "tijera")):
+        print(f"Perdiste {nombre} :( ¡Inténtalo de nuevo!")
+    # tijera gana ante papel:
+    elif ((eleccionCompu == "papel") & (eleccionUsuario == "tijera")):
+        print(f"¡Ganaste {nombre}!")
 
-# piedra pierde ante papel:
-elif ((eleccionCompu == "papel") & (eleccionUsuario == "piedra")):
-    print(f"Perdiste {nombre} :( ¡Inténtalo de nuevo!")
-# piedra gana ante tijera:
-elif ((eleccionCompu == "tijera") & (eleccionUsuario == "piedra")):
-    print(f"¡Ganaste {nombre}!")
+    # piedra pierde ante papel:
+    elif ((eleccionCompu == "papel") & (eleccionUsuario == "piedra")):
+        print(f"Perdiste {nombre} :( ¡Inténtalo de nuevo!")
+    # piedra gana ante tijera:
+    elif ((eleccionCompu == "tijera") & (eleccionUsuario == "piedra")):
+        print(f"¡Ganaste {nombre}!")
 
-# Si el usuario escribió mal:
-else:
-    print(f"Escribiste mal tu elección {nombre}, intenta de nuevo.")
+    # Si el usuario escribió mal:
+    else:
+        print(f"Escribiste mal tu elección {nombre}, intenta de nuevo.")
+
+    playAgain = input("¿Quieres volver a jugar? (si/no): \n").lower()
