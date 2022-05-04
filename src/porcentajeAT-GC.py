9072f1f4f5d80f454a28d1dfd425fc2664cdcf30
Diana <diana.delgado.gtz@gmail.com>
2022-05-03 23:50:56 -0500

    Se agregan excepciones en porcentajeAT-GC.py

--- src/porcentajeAT-GC.py
+++ src/porcentajeAT-GC.py
@@ -2,7 +2,7 @@
 NAME: 
     Porcentaje de AT y GC
 VERSION:
-    1
+    2
 AUTOR: 
     Diana Delgado Gutierrez
 DESCRIPTION: 
@@ -22,17 +22,25 @@ OUTPUT:
 # Pedir el nombre y ruta del archivo con la secuecia de DNA para leerlo.
 print("¿Cuál es el nombre y ruta del archivo de DNA para calcular el porcentaje de AT y GC?")
 archivoNombre = input()
-secuencia = open(archivoNombre)
-dna = secuencia.read()
 
-# Calcular porcentaje
-# Contar A+T entre el total de la secuencia por cien.
-print(f"La secuencia es: {dna}")
-porcentajeAT = (dna.count('A') + dna.count('T')) / len(dna) * 100
-porcentajeCG = (dna.count('C') + dna.count('G')) / len(dna) * 100
-print(f"Archivo de la secuencia: {archivoNombre}")
-print(
-    f"Porcentaje de AT: {porcentajeAT}% \n Porcentaje de CG: {porcentajeCG}%")
-
-# Cerrar archivo
-secuencia.close()
+# Checar que la ruta sea viable.
+try:
+    secuencia = open(archivoNombre)
+    dna = secuencia.read().upper()
+    try:
+        # Comprobar que la secuencia es una secuencia.
+        if (dna.isdigit() == True):
+            raise ValueError('No hay una secuencia de ADN')
+        # Calcular porcentaje
+        # Contar A+T entre el total de la secuencia por cien.
+        print(f"La secuencia es: {dna}")
+        porcentajeAT = (dna.count('A') + dna.count('T')) / len(dna) * 100
+        porcentajeCG = (dna.count('C') + dna.count('G')) / len(dna) * 100
+        print(f"Archivo de la secuencia: {archivoNombre}")
+        print(
+            f"Porcentaje de AT: {porcentajeAT}%\n Porcentaje de CG: {porcentajeCG}%")
+    finally:
+        # Cerrar archivo
+        secuencia.close()
+except IOError as exception:
+    print('No se pudo encontrar el archivo: ' + exception.strerror)
