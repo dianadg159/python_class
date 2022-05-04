6581bb131a251b2fc17ea27e7cbf6d64e69764a0
Diana <diana.delgado.gtz@gmail.com>
2022-05-03 23:15:15 -0500

    Se corrige código eliminarAdapt.py

--- src/eliminarAdapt.py
+++ src/eliminarAdapt.py
@@ -3,7 +3,7 @@ NAME
     Eliminar adaptador de secuencias
 
 VERSION
-    1.0.1
+    2
 
 AUTOR
     Diana Delgado Gutierrez
@@ -28,19 +28,6 @@ OUTPUT
     results/4_output_noAdapters.txt
 
 '''
-# Crear el archivo de la secuencia.
-archivo = open("data/4_input_adapters.txt", "w")
-linea1 = "ATTCGATTATAAGCTCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATC\n"
-linea2 = "ATTCGATTATAAGCACTGATCGATCGATCGATCGATCGATGCTATCGTCGT\n"
-linea3 = "ATTCGATTATAAGCATCGATCACGATCTATCGTACGTATGCATATCGATATCGATCGTAGTC \n"
-linea4 = "ATTCGATTATAAGCACTATCGATGATCTAGCTACGATCGTAGCTGTA\n"
-linea5 = "ATTCGATTATAAGCACTAGCTAGTCTCGATGCATGATCAGCTTAGCTGATGATGCTATGCA\n"
-archivo.write(linea1)
-archivo.write(linea2)
-archivo.write(linea3)
-archivo.write(linea4)
-archivo.write(linea5)
-archivo.close()
 try:
     # Leer el archivo línea por línea.
     contenido = open("data/4_input_adapter.txt", "r")
