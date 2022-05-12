85bc49f16cfff0e93c2f9b261453b850f2e471e3
Diana <diana.delgado.gtz@gmail.com>
2022-05-11 19:37:50 -0500

    Se añaden argument parser a porcentajeAT-GC.py

--- src/porcentajeAT-GC.py
+++ src/porcentajeAT-GC.py
@@ -2,7 +2,7 @@
 NAME: 
     Porcentaje de AT y GC
 VERSION:
-    2
+    3
 AUTOR: 
     Diana Delgado Gutierrez
 DESCRIPTION: 
@@ -14,18 +14,37 @@ ARGUMENTS:
 SOFTWARE REQUIREMENTS: 
     python 3.10
 INPUT: 
-    string
+    path del documento con secuencia
 OUTPUT: 
-    string
+    porcentaje de AT, GC
 '''
+
+import argparse
+
+parser = argparse.ArgumentParser(
+    description="Script que calcula la cantidad de AT en una secuencia.")
+
+parser.add_argument("-i", "--input",
+                    metavar="path/to/file",
+                    help="File with gene sequences",
+                    required=True)
+
+parser.add_argument("-o", "--output",
+                    help="Path for the output file",
+                    required=False)
+
+parser.add_argument("-r", "--round",
+                    help="number of digits to rouond",
+                    type=int,
+                    required=False)
 
 # Pedir el nombre y ruta del archivo con la secuecia de DNA para leerlo.
 print("¿Cuál es el nombre y ruta del archivo de DNA para calcular el porcentaje de AT y GC?")
-archivoNombre = input()
+args = parser.parse_args()
 
 # Checar que la ruta sea viable.
 try:
-    secuencia = open(archivoNombre)
+    secuencia = open(args.input, "r")
     dna = secuencia.read().upper()
     try:
         # Comprobar que la secuencia es una secuencia.
@@ -36,7 +55,7 @@ try:
         print(f"La secuencia es: {dna}")
         porcentajeAT = (dna.count('A') + dna.count('T')) / len(dna) * 100
         porcentajeCG = (dna.count('C') + dna.count('G')) / len(dna) * 100
-        print(f"Archivo de la secuencia: {archivoNombre}")
+        print(f"Archivo de la secuencia: {args.input}")
         print(
             f"Porcentaje de AT: {porcentajeAT}%\n Porcentaje de CG: {porcentajeCG}%")
     finally:
