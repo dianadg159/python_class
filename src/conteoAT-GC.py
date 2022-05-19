277e8690d1d7b412c98f296736cb87bec10ab4b7
Diana <diana.delgado.gtz@gmail.com>
2022-05-19 12:34:37 -0500

    Se agregan funciones

--- src/conteoAT-GC.py
+++ src/conteoAT-GC.py
@@ -1,21 +1,21 @@
 '''
-NAME: 
+NAME:
     Conteo de A,C,G y T
-VERSION: 
-    1.1
-AUTOR: 
+VERSION:
+    2
+AUTOR:
     Diana Delgado Gutierrez
-DESCRIPTION: 
+DESCRIPTION:
     Programa que cuente el total de A,C,G y T que hay en una secuencia de DNA introducida por el usuario.
-USAGE: 
+USAGE:
     ejercicio
-ARGUMENTS: 
+ARGUMENTS:
     string
-SOFTWARE REQUIREMENTS: 
+SOFTWARE REQUIREMENTS:
     python 3.10
-INPUT: 
+INPUT:
     string
-OUTPUT: 
+OUTPUT:
     string
 '''
 
@@ -28,5 +28,23 @@ if (dna.count('A') == 0 & dna.count('T') == 0 & dna.count('C') == 0 & dna.count(
     raise ValueError('Lo que introduciste no es una secuencia de ADN.')
 
 # La cantidad de A, T, C y G.
-print(
-    f"El total por base es: \n A:{dna.count('A')} T:{dna.count('T')}  C:{dna.count('C')}  G:{dna.count('G')}")
+
+
+def get_AT_content(dna, decimales):
+    length = len(dna)
+    a_count = dna.upper().count('A')
+    t_count = dna.upper().count('T')
+    at_content = (a_count + t_count) / length
+    return str(round(at_content, decimales))
+
+
+def get_GC_content(dna, decimales):
+    length = len(dna)
+    g_count = dna.upper().count('G')
+    c_count = dna.upper().count('C')
+    gc_content = (c_count + g_count) / length
+    return str(round(gc_content, decimales))
+
+
+print("Contenido de AT: " + get_AT_content(dna, 2))
+print("Contenido de GC: " + get_GC_content(dna, 2))
