9dc2258c5dd74a9b6d1b010d5f950c1633f13be7
Diana <diana.delgado.gtz@gmail.com>
2022-06-09 07:57:55 -0500

    Correccion de errores pequeños

--- src/porcentajeAT-GC.py
+++ src/porcentajeAT-GC.py
@@ -3,7 +3,7 @@ NAME:
     PorcentajeAT-GC.py
     
 VERSION:
-    3.1
+    3.1.1
     
 AUTOR: 
     Diana Delgado Gutierrez
@@ -36,7 +36,7 @@ OUTPUT:
 import argparse
 
 parser = argparse.ArgumentParser(
-    description="Script que calcula la cantidad de AT en una secuencia.")
+    description="Script que calcula el porcentaje de AT-GC en una secuencia.")
 
 parser.add_argument("-i", "--input",
                     metavar="path/to/file",
@@ -56,38 +56,40 @@ args = parser.parse_args()
 
 # Checar que la ruta sea viable.
 try:
-    
+
     secuencia = open(args.input, "r")
     dna = secuencia.read().upper()
-    
+
     try:
         # Comprobar que la secuencia es una secuencia.
         if (dna.isdigit() == True):
             raise ValueError('No hay una secuencia de ADN')
-            
+
         # Calcular porcentaje
         # Contar A+T entre el total de la secuencia por cien.
         print(f"La secuencia es: {dna}")
         porcentajeAT = (dna.count('A') + dna.count('T')) / len(dna) * 100
         porcentajeAT = round(porcentajeAT, args.round)
-        
+
         porcentajeCG = (dna.count('C') + dna.count('G')) / len(dna) * 100
         porcentajeCG = round(porcentajeCG, args.round)
-        
+
+        # Si quiere que guarde los resultados
         if args.output:
+            outputFile = open(args.output, "w")
             print(f"Archivo de la secuencia: {args.input}")
             print(
                 f"Porcentaje de AT: {porcentajeAT}%\n Porcentaje de CG: {porcentajeCG}%",
-            file=args.output)
-            
+                file=outputFile)
+
         else:
             print(f"Archivo de la secuencia: {args.input}")
             print(
                 f"Porcentaje de AT: {porcentajeAT}%\n Porcentaje de CG: {porcentajeCG}%")
-        
+
     finally:
         # Cerrar archivo
         secuencia.close()
-        
+
 except IOError as exception:
     print('No se pudo encontrar el archivo: ' + exception.strerror)
