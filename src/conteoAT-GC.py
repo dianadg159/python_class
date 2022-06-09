a36289366462df40f0c199f25a5501c03bbfda11
Diana <diana.delgado.gtz@gmail.com>
2022-06-09 07:38:58 -0500

    Implementa argparse

--- src/conteoAT-GC.py
+++ src/conteoAT-GC.py
@@ -1,27 +1,47 @@
 '''
 NAME:
     Conteo de A,C,G y T
+    
 VERSION:
-    2
+    2.1
+    
 AUTOR:
     Diana Delgado Gutierrez
+    
 DESCRIPTION:
     Programa que cuente el total de A,C,G y T que hay en una secuencia de DNA introducida por el usuario.
+    
 USAGE:
-    ejercicio
+    py conteoAT-GC.py [-h] -s SEC
+    
 ARGUMENTS:
-    string
+    -h, --help         show this help message and exit
+    -s SEC, --sec SEC  Secuencia de adn.
+    
 SOFTWARE REQUIREMENTS:
     python 3.10
+    
 INPUT:
-    string
+    en pantalla: string de secuencia de adn.
+    
 OUTPUT:
-    string
+    en pantalla: porcentaje de AT y GC
+    
 '''
+import argparse
 
 # Pedir la secuencia de DNA al usuario.
-print("Introduce una secuencia de adn para saber si cantidad de A, T, C y G: \n")
-dna = input().upper()
+parser = argparse.ArgumentParser(
+    description="Script que cuenta contenido de AT y GC en una secuencia.")
+
+parser.add_argument("-s", "--sec",
+                    help="Secuencia de adn.",
+                    type=str.upper,
+                    required=True)
+
+# Pedir los datos
+argumentos = parser.parse_args()
+dna = argumentos.sec
 
 # Generar un ValueError cuando no se da una secuencia de ADN.
 if (dna.count('A') == 0 & dna.count('T') == 0 & dna.count('C') == 0 & dna.count('G') == 0):
@@ -31,17 +51,37 @@ if (dna.count('A') == 0 & dna.count('T') == 0 & dna.count('C') == 0 & dna.count(
 
 
 def get_AT_content(dna, decimales):
+    '''
+    Obtiene el porcentaje de AT en una secuencia.
+
+    Parámetros: 
+        dna(string): Secuencia de adn.
+        decimales(int): Número de decimales a redondear.
+
+    Returns:
+        Porcentaje de AT(string)
+    '''
     length = len(dna)
-    a_count = dna.upper().count('A')
-    t_count = dna.upper().count('T')
+    a_count = dna.count('A')
+    t_count = dna.count('T')
     at_content = (a_count + t_count) / length
     return str(round(at_content, decimales))
 
 
 def get_GC_content(dna, decimales):
+    '''
+    Obtiene el porcentaje de GC en una secuencia.
+
+    Parámetros: 
+        dna(string): Secuencia de adn.
+        decimales(int): Número de decimales a redondear.
+
+    Returns:
+        Porcentaje de GC(string)
+    '''
     length = len(dna)
-    g_count = dna.upper().count('G')
-    c_count = dna.upper().count('C')
+    g_count = dna.count('G')
+    c_count = dna.count('C')
     gc_content = (c_count + g_count) / length
     return str(round(gc_content, decimales))
 
