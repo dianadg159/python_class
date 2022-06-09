b243dc4a8df6cb67b11f46932b4dd7b726f9aad8
Diana <diana.delgado.gtz@gmail.com>
2022-06-09 07:22:17 -0500

    Update dna-FASTA.py

--- src/dna-FASTA.py
+++ src/dna-FASTA.py
@@ -1,32 +1,75 @@
 '''
 NAME: 
-  dna - FASTA
+  dna-FASTA
+  
 VERSION:
-  1
+  2
+  
 AUTOR: 
   Diana Delgado Gutierrez
+  
 DESCRIPTION: 
   Generar un archivo FASTA a partir de un archivo .txt existente en el directorio data.
+  
 USAGE: 
-  ejercicio
-ARGUMENTS: 
-  string
+  dna-FASTA.py [-h] -i INPUT -o OUTPUT [-n NAME]
+  
+ARGUMENTS:
+  -h, --help            show this help message and exit
+  -i INPUT, --input INPUT
+                        Path del archivo con secuencia de adn     
+  -o OUTPUT, --output OUTPUT
+                        Path del archivo en formato fasta
+  -n NAME, --name NAME  Nombre que poner a la secuencia
+  
 SOFTWARE REQUIREMENTS:
   python 3.10
+  
 INPUT: 
-  archivo dna.txt
+  data/archivo.txt
+  
 OUTPUT:
-  archivo dns.fasta
+  results/archivo.fasta
+  
 '''
+import argparse
 
-# Copiar el contenido de dna.txt
-contenido = open("data/dna.txt")
+# Pedir datos
+parser = argparse.ArgumentParser(
+    description="Script que pasa un archivo de secuancia adn a formato fasta.")
+
+parser.add_argument("-i", "--input",
+                    help="Path del archivo con secuencia de adn",
+                    required=True)
+parser.add_argument("-o", "--output",
+                    help="Path del archivo en formato fasta",
+                    required=True)
+parser.add_argument("-n", "--name",
+                    help="Nombre que poner a la secuencia",
+                    required=False)
+
+# Pedir los datos
+argumentos = parser.parse_args()
+
+# Copiar el contenido del archivo del usuario
+input = argumentos.input
+contenido = open(input)
 contenidoDna = contenido.read()
 dna = contenidoDna.rstrip("\n")
 contenido.close()
 
 # Crear el archivo FASTA y copiar el .txt
 # Escribir la ubicación en la que queremos guardar el FASTA.
-fastaArchivo = open("results/dna.fasta", "w")
-fastaArchivo.write(">sequence_name \n" + dna)
-fastaArchivo.close()
+
+
+def archivoFasta(argumentos):
+    output = argumentos.output
+    fastaArchivo = open(output, "w")
+    if argumentos.name:
+        fastaArchivo.write(">" + argumentos.name + "\n" + dna)
+    else:
+        fastaArchivo.write(">sequence_name \n" + dna)
+    fastaArchivo.close()
+
+
+archivoFasta(argumentos)
