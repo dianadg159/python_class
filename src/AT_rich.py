11281604a619befa3b3e4a721be70ceea949e886
Diana <diana.delgado.gtz@gmail.com>
2022-08-30 21:53:32 -0500

    Poner docstrings

--- src/AT_rich.py
+++ src/AT_rich.py
@@ -3,7 +3,7 @@ NAME
     Regiones ricas en AT
 
 VERSION
-    1.0
+    1.1
     
 AUTOR
 	Diana Delgado Gutiérrez
@@ -17,10 +17,12 @@ CATEGORY
     DNA
     
 USAGE
-    
+    Python .\src\rich_at.py [-h] -f path/to/file [-s SEARCH]
+    python .\src\AT_rich.py -f data/dna.seq -s 5
     
 ARGUMENTS
-    -f archivo con secuencia de ADN a leer.
+    -h, --help
+    -f, --file  archivo con secuencia de ADN a leer.
     -s [número] tamaño de la región rica en AT.
     
 SOFTWARE REQUIREMENTS
@@ -42,10 +44,11 @@ parser = argparse.ArgumentParser(
     description="Script que detecta regiones ricas en AT.")
 parser.add_argument("-f", "--file",
                     help="Atchivo con la secuencia de ADN.",
-                    required=True)
+                    required=False)
 parser.add_argument("-s", "--size",
                     help="Longitud de la región con AT.",
-                    required=False)
+                    required=False,
+                    default="13")
 
 # Asignar variables al input.
 argumentos = parser.parse_args()
@@ -60,6 +63,16 @@ seqdna = dna.read().upper()
 
 
 def revise(seqdna):
+    '''
+    Checa si hay errores en el archivo de secuencia
+    o si no encuentra ninguno.
+        Parameters:
+            seqdna (str): Secuencia de adn del archivo
+                            introducido por usuario.
+        Returns:
+            False: No hubo errores.
+            True: Hubo errores.
+    '''
     errores = re.finditer("[^ATGC]+", seqdna)
     if (errores):
         # Imprimir los errores.
@@ -71,7 +84,17 @@ def revise(seqdna):
 
 
 # Encontrar las regiones ricas en AT del tamaño size.
-def rich_at_regions(seqdna, size=13):
+def rich_at_regions(seqdna, size):
+    '''
+    Encuentra regiones ricas en AT.
+    Default a 13, el mínimo para decir que una region es 
+    rica en AT.
+        Parameters:
+            seqdna (str): Secuencia de adn del archivo
+                            introducido por usuario.
+            size (str): Numero de longitud mínima de 
+                            región rica en AT.
+    '''
     at_rich = re.finditer("(A+|T+){" + size + ",}", seqdna, re.IGNORECASE)
     matches = len(
         [*re.finditer("(A+|T+){" + size + ",}", seqdna, re.IGNORECASE)])
