07d546e0f146470b1584cdd99009f59db7960edc
Diana <diana.delgado.gtz@gmail.com>
2022-09-04 23:28:50 -0500

    Primera versión de ADN a Prot

---
+++ src/RNA_to_prot.py
@@ -0,0 +1,66 @@
+'''
+NAME: 
+    RNA to Protein
+    
+VERSION:
+    1.0
+    
+AUTOR: 
+    Diana Delgado Gutierrez
+    
+DESCRIPTION: 
+    Programa que traduce una secuencia de ARN a una cadena 
+    de aminoácidos. 
+    
+USAGE: 
+    python RNA_to_prot.py [-h] -s secuenciaRNA
+    
+ARGUMENTS: 
+    -h, --help            show this help message and exit
+    -s, --string          string de RNA a traducir
+                        
+SOFTWARE REQUIREMENTS: 
+    python 3.10
+    
+INPUT: 
+    secuencia de RNA
+    
+OUTPUT: 
+    en pantalla: cadema de aminoácidos
+'''
+# Importando librería
+import argparse
+
+# Input
+parser = argparse.ArgumentParser(
+    description="Script que traduce secuencias de ARN.")
+parser.add_argument("-s", "--string",
+                    help="String con secuencia de ARN.",
+                    required=True)
+
+# Asignar variables al input.
+argumentos = parser.parse_args()
+rna = argumentos.string
+
+
+# Hacer un diccionario
+gencode = {
+    'ATA': 'I', 'ATC': 'I', 'ATT': 'I', 'ATG': 'M', 'ACA': 'T',
+    'ACC': 'T', 'ACG': 'T', 'ACT': 'T', 'AAC': 'N', 'AAT': 'N',
+    'AAA': 'K', 'AAG': 'K', 'AGC': 'S', 'AGT': 'S', 'AGA': 'R',
+    'AGG': 'R', 'CTA': 'L', 'CTC': 'L', 'CTG': 'L', 'CTT': 'L',
+    'CCA': 'P', 'CCC': 'P', 'CCG': 'P', 'CCT': 'P', 'CAC': 'H',
+    'CAT': 'H', 'CAA': 'Q', 'CAG': 'Q', 'CGA': 'R', 'CGC': 'R',
+    'CGG': 'R', 'CGT': 'R', 'GTA': 'V', 'GTC': 'V', 'GTG': 'V',
+    'GTT': 'V', 'GCA': 'A', 'GCC': 'A', 'GCG': 'A', 'GCT': 'A',
+    'GAC': 'D', 'GAT': 'D', 'GAA': 'E', 'GAG': 'E', 'GGA': 'G',
+    'GGC': 'G', 'GGG': 'G', 'GGT': 'G', 'TCA': 'S', 'TCC': 'S',
+    'TCG': 'S', 'TCT': 'S', 'TTC': 'F', 'TTT': 'F', 'TTA': 'L',
+    'TTG': 'L', 'TAC': 'Y', 'TAT': 'Y', 'TAA': '_', 'TAG': '_',
+    'TGC': 'C', 'TGT': 'C', 'TGA': '_', 'TGG': 'W'}
+
+# Hacer una lista de codones.
+codones = [rna[i:i + 3] for i in range(0, len(rna), 3)]
+
+# LLamar al diccionario
+peptido = [gencode.get(codon) for codon in codones]
