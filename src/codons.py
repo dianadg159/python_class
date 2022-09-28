3024ee62835105fad8a8aef7f26382b05226bc31
Diana <diana.delgado.gtz@gmail.com>
2022-09-28 17:48:25 -0500

    Ejercicio 2 avanzado

--- src/codons.py
+++ src/codons.py
@@ -1,9 +1,9 @@
 '''
 NAME
     Fasta to codons
-    
+
 VERSION
-    1.1
+    2.0
 
 AUTOR
 	Diana :)
@@ -33,7 +33,8 @@ OUTPUT
 #! /usr/bin/env python34
 # Importar librería
 from Bio import SeqIO
-from Bio import SeqUtils
+# from Bio import SeqUtils
+from Bio.Seq import Seq
 
 # parsear el archivo fasta en un diccionario
 dict = SeqIO.to_dict(SeqIO.parse("data/seq.nt.fa", "fasta"))
@@ -42,19 +43,26 @@ id_dict = dict.keys()
 # Encontrar el primer marco de lectura
 for id in id_dict:
     # Codón de inicio
-    start_codon = SeqUtils.nt_search(str(dict[id].seq), "TAC")
+    # start_codon = SeqUtils.nt_search(str(dict[id].seq), "TAC")
     # Checar que haya marco de lectura
-    if len(start_codon) == 1:
-        # Si no hay marco de lectura que siga con el siguiente record
-        continue
-    else:
-        # Posición del primer codón
-        start = start_codon[1]
-        # Separar codones
+    # if len(start_codon) == 1:
+    # Si no hay marco de lectura que siga con el siguiente record
+    # continue
+    # else:
+    # Posición del primer codón
+    for j in range(3):
+        # Separar codones en leading y en lagging
+        complement = [str(dict[id].seq.complement()[i:i + 3])
+                      for i in range(j, len(str(dict[id].seq)), 3)]
         codones = [str(dict[id].seq[i:i + 3])
-                   for i in range(start, len(str(dict[id].seq)), 3)]
-        if (len(dict[id].seq[start:]) % 3) != 0:
+                   for i in range(j, len(str(dict[id].seq)), 3)]
+        # Quitar los codones que no son tripletes
+        if (len(dict[id].seq[j:]) % 3) != 0:
             codones.pop()
-        # imprimirlos codones separados
-        print(f">{id}")
+            complement.pop()
+        # imprimirlos codones separados en formato fasta
+        print(f">{id} en marco {j+1}")
         print(*codones)
+        print(f">{id} complemento en marco {j+1}")
+        print(*complement)
+        print("\n")
